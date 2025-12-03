cc696ecb271a58c3cf11dfbb0fde04513aa69951
Antonio Bravo <94081252+Abravova@users.noreply.github.com>
2025-12-02 17:00:11 -0800

    Update A8.rkt
    Possible direction not too sure if its right. Only did 2.5hrs. Can do more later.

--- A8.rkt
+++ A8.rkt
@@ -1,1 +1,22 @@
 #lang algol60
+
+begin
+  comment data definitions, values for ExprC;
+  integer TAGNumC; real n;
+  integer TAGIdC; integer id;
+  integer TAGIfC; integer testval, thenval, otherwiseval;
+  integer TAGLamC; integer argPtr, bodyPtr;
+  integer TAGAppC; integer funPtr, argsPtr;
+  integer TAGStrC; integer strPtr;
+  integer TAGPrimC; integer primCode;
+
+  comment each tag is assigned a number;
+  TAGNumC  := 1;
+  TAGIdC   := 2;
+  TAGIfC   := 3;
+  TAGLamC  := 4;
+  TAGAppC  := 5;
+  TAGStrC  := 6;
+  TAGPrimC := 7;
+
+end
